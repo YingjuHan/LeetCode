71e249d65811a93ff248ab8f8d20a6201fc45f53
“hanyingju” <hanyingju@live.cn>
2021-11-06 15:54:21 +0800

    2021-11-6 =

---
+++ 5.最长回文子串.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+
+#include <string>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+// @lc code=end
+
---
+++ 704.二分查找.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=cpp
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1; // 定义target在左闭右闭的区间里，[left, right]
+        while (left <= right) {
+            int middle = left + (right - left);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
